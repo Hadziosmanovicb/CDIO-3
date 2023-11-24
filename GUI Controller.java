43cddc3659972aef52bca7d3e93a7e3377b03915
Hadziosmanovicb <144700262+Hadziosmanovicb@users.noreply.github.com>
2023-11-24 18:55:15 +0100

    GUI Controller.java

---
+++ GUI Controller.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class GUI_Controller {
+    private Game game;
+    private JFrame frame;
+
+    public GUI_Controller() {
+        game = new Game(new Board(), new Player[] { 
+        initializeWindow();
+    }
+
+    
