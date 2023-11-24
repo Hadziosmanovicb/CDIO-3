d80e06bdb827ce0844761d03f18b6f3a7433171c
Hadziosmanovicb <144700262+Hadziosmanovicb@users.noreply.github.com>
2023-11-24 19:00:00 +0100

    Translate.java

---
+++ Translate.java
@@ -0,0 +1,10 @@
+public class Translate {
+    private static Translate instance;
+
+    private static String fileLocation = "/languages/";
+    private static String lang = "da_DK";
+
+    private HashMap<String, String> translations;
+
+
+  
