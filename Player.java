bde038b07485785e218d085257d21380e0225018
Hadziosmanovicb <144700262+Hadziosmanovicb@users.noreply.github.com>
2023-11-24 18:51:51 +0100

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,18 +1,94 @@
 public class Player {
-    private String name;
-    private int money;
-    private int position;
-    private boolean isBankrupt;
-    private Token token;
 
-    public Player(String name, Token token) {
-        this.name = name;
-        this.money = 20; // Startpenge (kan justeres afhængigt af antal spillere)
-        this.position = 0; // Startposition (START)
-        this.isBankrupt = false;
-        this.token = token;
+    private GUI_Player guiPlayer;
+    private String name;
+    private Account Account;
+    private Type type = Type.CAT;
+    private int prisonCards;
+
+
+    
+   
+    private TurnAction turnAction;
+   
+    public enum Type {
+        CAT,
+        DOG,
+        CAR,
+        BOAT;
+
+         
+        public static Type toEnum(String name)
+        {
+            try {
+                return Type.valueOf(name.toUpperCase());
+            } catch (Exception exception) {
+                return Type.CAT;
+            }
+        }
+
+       
+        public static String[] valuesToString()
+        {
+            Type[] types = Type.values();
+            String[] typesString = new String[types.length];
+
+            for (int i = 0; i < types.length; i++) {
+                typesString[i] = types[i].toString();
+            }
+
+            return typesString;
+        }
     }
 
-    // Implementer getter- og setter-metoder efter behov
-    // Implementer en metode til at flytte spillerens position baseret på terningekast
-}
+    
+    
+    public Player(String name)
+    {
+        this(name, 0);
+    }
+
+    
+   
+    public Player(String name, int startBalance)
+    {
+        this.name = name;
+
+        this.stash = new Stash(startBalance);
+        this.guiPlayer = new GUI_Player(this.name, this.stash.getAmount());
+        this.prisonCards = 0;
+    }
+
+    public String getName()
+    {
+        return this.name;
+    }
+
+    
+    public int getBalance()
+    {
+        return this.stash.getAmount();
+    }
+
+   
+    public void addBalance(int balance)
+    {
+        this.stash.addAmount(balance);
+        this.guiPlayer.setBalance(this.stash.getAmount());
+    }
+
+    
+    public int getPrisonCard () {return this.prisonCards; }
+
+    
+    public int addPrisonCard (int numbersOfPrisonCards)
+    {
+        this.prisonCards += numbersOfPrisonCards;
+
+        return this.prisonCards;
+    }
+
+    /
+    public int removePrisonCard (int numbersOfPrisonCards)
+    {
+   
